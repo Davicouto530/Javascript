261b069e2d950ec8a82783b25c6abd3524755c41
davicouto530 <coutodavi530@gmail.com>
2025-01-22 17:07:23 -0300

    operadores

---
+++ exercíciosSenac/fundamentos/operadores.js
@@ -0,0 +1,29 @@
+/**
+ * Operadores 
+ * Aritiméticos
+ * Aritiméticos de atribuição
+ * Comparação
+ * Lógicos
+ */
+
+console.clear
+
+console.log("Operadores artiméticos")
+console.log(" + | soma")
+console.log(" - | subtração")
+console.log(" * | multiplicação")
+console.log(" / | divisão")
+console.log(" % | resto da divisão")
+console.log("---------------------------------")
+
+console.log("Operadores aritiméticos de atribuição")
+console.log(" += | soma e atribuição a variável")
+console.log(" -= | subtração e atribuição a variável")
+console.log(" *= | multiplicação e atribuição a variável")
+console.log(" /= | divisão e atribuição a variável")
+console.log(" ++ | somar 1 ao resultado")
+console.log(" -- | subtrair 1 ao resultado")
+console.log("---------------------------------")
+
+
+console.log("---------------------------------")
