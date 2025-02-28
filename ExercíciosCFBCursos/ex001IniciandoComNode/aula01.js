f2940dcfc1d1a0075a42b6da863e83edd49fdde8
Davi do Couto <coutodavi530@gmail.com>
2025-02-28 18:16:03 -0300

    Iniciando o curso de Javascript pelo CFB Cursos.

---
+++ ExercíciosCFBCursos/ex001IniciandoComNode/aula01.js
@@ -0,0 +1,7 @@
+//Comentários de linha
+
+/**
+ * Comentário de bloco de código
+ */
+
+console.log('Alô mundo!')
---
+++ ExercíciosCFBCursos/ex002ModoStrict/aula02.js
@@ -0,0 +1,6 @@
+"use strict" //Modo estrito, serve para deixar o código mais limpo e rigoroso
+
+let nome="Bruno"//Não pode criar variável sem o "let" nesse modo e etc.
+console.log("CFB cursos")
+console.log(nome)
+console.log("Nome: " + nome)
