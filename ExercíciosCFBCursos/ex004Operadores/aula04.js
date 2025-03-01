513cdac14a13c918719967bef21a2353e47734e8
Davi do Couto <coutodavi530@gmail.com>
2025-03-01 11:34:49 -0300

    Operadores em Javascript pelo CFB cursos.

---
+++ ExercíciosCFBCursos/ex004Operadores/aula04.js
@@ -0,0 +1,41 @@
+"use strict"
+
+/**
+ * Soma -> +
+ * Subtração -> -
+ * Multiplicação -> *
+ * Divisão -> /
+ * Resto da divisão -> %
+ * Incremento -> ++
+ * Decremento -> -- 
+ * Incremento atribuindo -> +=
+ * Decremento atribuindo -> -=
+ */
+
+let num1=0,num2=0,res=0
+
+num1 = 2
+num2 = 15
+
+num1++//Incrementando mais 1 na variável "num1" 
+console.log(num1)
+
+num2--//Decrementando 1 na variável "num2" 
+console.log(num2)
+
+res = (num1 + num2) * 2//Ordem de precedencia, o () indica qual vai ser feito primeiro
+
+console.log(res)
+
+console.log(num2 / num1)//Divisão
+console.log(num2 % num1)//Resto da divisão
+
+console.log(num1)
+num1 += 5 //Atribuindo e incrementando na variável "num1"
+console.log(num1)
+
+console.log(num2)
+num1 *= 5 //Atribuindo e multiplicando na variável "num2", pode fazer com todos os sinais de matemática
+console.log(num2)
+
+
---
+++ ExercíciosCFBCursos/ex005OperadoresRelacionais/aula05.js
@@ -0,0 +1,30 @@
+"use strict"
+
+/**
+ * Maior -> >
+ * Maior ou igual -> >=
+ * Menor -> <
+ * Menor ou igual -> <=
+ * Igual -> ==
+ * Not -> !
+ * Diferente -> !=
+ */
+
+let num1 = 10
+let num2 = 5
+let num3 = 10
+
+console.log(num1 > num2)//"num1" é maior que "num2"
+
+console.log(num1 < num2)//"num1" é menor que "num2"
+
+console.log(num1 >= num3)//"num1" é maior ou igual que "num3"
+
+console.log(num1 == num3)//"num1" é igual que "num3"
+
+console.log(!(num1 == num3))
+//Operador de not "!", ele inverte o valor, se for verdadeiro, inverte para falso, mesma coisa se for falso, inverte para verdadeiro
+
+console.log(num1 != num3)
+//Operador de diferente, "num1" diferente de "num3", vai da falso, porque são iguais
+
