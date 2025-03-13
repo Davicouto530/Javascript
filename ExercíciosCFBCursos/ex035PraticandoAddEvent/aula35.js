7e6bfe319efedf1c8f8be3e2fc0ac77729664d48
Davi do Couto <coutodavi530@gmail.com>
2025-03-13 20:46:14 -0300

    Praticando addEventListener no CFB Cursos.

--- ExercíciosCFBCursos/ex035PraticandoAddEvent/aula35.js
+++ ExercíciosCFBCursos/ex035PraticandoAddEvent/aula35.js
@@ -7,7 +7,7 @@
 //Pegando todos os IDS do HTML
 const caixa1 = document.querySelector("#caixa1");
 const caixa2 = document.querySelector("#caixa2");
-const btn = document.querySelector("#btn_copiar");
+const btn_transferir = document.querySelector("#btn_transferir");
 
 //Pegando todas as tags que tem a class "curso" e fazendo um array
 const todosCursos = [...document.querySelectorAll(".curso")];
@@ -15,22 +15,27 @@ const todosCursos = [...document.querySelectorAll(".curso")];
 //Percorrendo todas as tags e colocando um evento de "click" em cada uma
 todosCursos.map((el) => {
     el.addEventListener("click", (evt) => {
-        const curso = evt.target;
+        const curso = evt.target;//O ".target" verifica se a tag foi clicada
         curso.classList.toggle("selecionado");
         //O "toggle" verifica se o elemento tem a class, se
         //não tiver ele adiciona a class, se já tiver, ele remove
     });
 });
 
-//Colocando um evento de "click" no que está dentro de "bnt"
-btn.addEventListener("click", () => {
+//Colocando um evento de "click" no que está dentro de "btn"
+btn_transferir.addEventListener("click", () => {
     //Quando clicar cria um array de todos os curso selecionados, que foram clicados
     const cursosSelecionados = [...document.querySelectorAll(".selecionado")];
+    const cursosNaoSelecionados = [...document.querySelectorAll(".curso:not(.selecionado)")];//Todos os cursos com a class ".curso" que não estão selecionados com a class ".selecionado"
+    
     cursosSelecionados.map((el) => {
         // Move cada elemento selecionado para dentro da div caixa2
         caixa2.appendChild(el);
     });
 
-    
+    cursosNaoSelecionados.map((el) => {
+        // Move cada elemento não selecionado para dentro da div caixa1
+        caixa1.appendChild(el);
+    });
 });
 
