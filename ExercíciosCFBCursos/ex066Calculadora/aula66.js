2f49f244d81791d62630c51770212e074e5daf24
Davi do Couto <coutodavi530@gmail.com>
2025-04-10 14:08:00 -0300

    Iniciando o JS da calculadora com o CFB Cursos.

--- ExercíciosCFBCursos/ex066Calculadora/aula66.js
+++ ExercíciosCFBCursos/ex066Calculadora/aula66.js
@@ -1,65 +1,22 @@
-"use strict";
+const teclasNum = [...document.querySelectorAll(".num")];
+const teclasOp = [...document.querySelectorAll(".op")];
+const teclaRes = document.querySelector(".res");
+const display = document.querySelector(".display");
+const ton = document.getElementById("ton");
+const tlimpar = document.getElementById("tlimpar");
 
-/* 
- * Abstract
- */
+teclasNum.forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        display.innerHTML += evt.target.innerHTML;
+    });
+});
 
-// A classe "abstract" não pode ser instanciada, ela só serve 
-//de base para outras classes, para usar como herança.
+teclasOp.forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        display.innerHTML += evt.target.innerHTML;
+    });
+});
 
-//Classe abstract
-class CarroPadrao {
-    constructor() {
-        //Verificando se foi criado algum objeto com a classe "CarroPadrao"
-        if (this.constructor == CarroPadrao) {
-            throw new TypeError("Está classe não pode ser instânciada");
-            //Se foi, vai da escrito esse erro
-        }
-
-        //Colocando como regra que todas as classes que recebe a classe "CarroPadrao"
-        //tem que ter o método ligar nelas
-        if (this.ligar === undefined) {
-            throw new TypeError("É obrigatório implementar o método ligar");
-        }
-
-        //Colocando mais um método obrigatório
-        if (this.desligar === undefined) {
-            throw new TypeError("É obrigatório implementar o método desligar");
-        }
-
-        this.portas = 4;
-        this.rodas = 4;//Criando propiedades padrao
-        this.ligado = false;
-    }
-}
-
-class Carro extends CarroPadrao {
-    constructor(nome, velMax) {
-        super();
-        this.nome = nome;
-        this.velMax = velMax;
-    }
-
-    info() {
-        // Exibe informações do carro no console.
-        console.log(this.nome);
-        console.log(this.velMax);
-        console.log(this.rodas);
-        console.log(this.portas);
-        console.log(this.ligado);
-        console.log("------------------");
-    }
-
-    //Obrigatório chamar esses métodos
-    ligar() {
-        this.ligado = true;
-    }
-    desligar() { 
-        this.deligado = false; 
-    }
-}
-
-const c1 = new Carro("Civic G9", 200); 
-
-// Exibe as informações já com as propriedades da classe abstract
-c1.info();
+tlimpar.addEventListener("click", (evt) => {
+    display.innerHTML = 0;
+})
