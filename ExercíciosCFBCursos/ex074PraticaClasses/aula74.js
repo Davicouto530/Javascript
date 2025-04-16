ed599cae0e6323cb387ba0a9c28d9ff64f41b42f
Davi do Couto <coutodavi530@gmail.com>
2025-04-16 11:34:54 -0300

    Criando a classe POO para o projeto com CFB Cursos.

--- ExercíciosCFBCursos/ex074PraticaClasses/aula74.js
+++ ExercíciosCFBCursos/ex074PraticaClasses/aula74.js
@@ -16,3 +16,43 @@ let larguraPalco = palco.offsetWidth;
 let alturaPalco = palco.offsetHeight;
 let bolas = [];//Array para armazenar as bolas
 let numBola = 0;
+
+//Classe que vai criar as bolinhas e movimentação 
+class Bola {
+    constructor(){
+        //Definindo tamanhos diferentes para a bolinha
+        this.tam = Math.floor(Math.random()*10)+10;
+
+        //Definindo cores diferentes para cada bolinha com "rgb"
+        this.r = Math.floor(Math.random()*255);
+        this.g = Math.floor(Math.random()*255);
+        this.b = Math.floor(Math.random()*255);
+
+        //Definindo lugares diferentes aonde as bolinhas pode nascer
+        this.px = Math.floor(Math.random()*(larguraPalco - this.tam));
+        this.py = Math.floor(Math.random()*(alturaPalco - this.tam));
+    }
+}
+
+//Toda vez que redimensionar a janela, executa a função
+window.addEventListener("resize", (evt) => {
+    //Pegando a largura e altura da janela
+    larguraPalco = palco.offsetWidth;
+    alturaPalco = palco.offsetHeight;
+});
+
+btn_add.addEventListener("click", (evt) => {
+    //Pegando o valor de bolinhas que quer ser colocadas
+    const qtde = txt_qtde.value;
+    for(let i = 0; i < qtde; i++){
+        //Instanciar novas bolinhas
+    }
+});
+
+btn_remover.addEventListener("click", (evt) => {
+    //Vai percorrer o array que as bolinhas vai ta 
+    //armazenadas e remover todas as bolinhas
+    bolas.map((bol) => {
+        //Remover a bolinha
+    })
+});
