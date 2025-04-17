4d6b70a9ad05abef9e865c48593b7d0dbc6753d9
Davi do Couto <coutodavi530@gmail.com>
2025-04-17 19:59:33 -0300

    Definindo propriedades na classe para a movimentação da bola com o CFB Cursos.

--- ExercíciosCFBCursos/ex074PraticaClasses/aula74.js
+++ ExercíciosCFBCursos/ex074PraticaClasses/aula74.js
@@ -19,18 +19,56 @@ let numBola = 0;
 
 //Classe que vai criar as bolinhas e movimentação 
 class Bola {
-    constructor(){
+    constructor(arrayBolas, palco) {
         //Definindo tamanhos diferentes para a bolinha
-        this.tam = Math.floor(Math.random()*10)+10;
+        this.tam = Math.floor(Math.random() * 10) + 10;
 
         //Definindo cores diferentes para cada bolinha com "rgb"
-        this.r = Math.floor(Math.random()*255);
-        this.g = Math.floor(Math.random()*255);
-        this.b = Math.floor(Math.random()*255);
+        this.r = Math.floor(Math.random() * 255);
+        this.g = Math.floor(Math.random() * 255);
+        this.b = Math.floor(Math.random() * 255);
 
         //Definindo lugares diferentes aonde as bolinhas pode nascer
-        this.px = Math.floor(Math.random()*(larguraPalco - this.tam));
-        this.py = Math.floor(Math.random()*(alturaPalco - this.tam));
+        this.px = Math.floor(Math.random() * (larguraPalco - this.tam));
+        this.py = Math.floor(Math.random() * (alturaPalco - this.tam));
+
+        //Definindo velocidades diferentes para cada bola
+        this.velx = Math.floor(Math.random() * 2) + 0.5;
+        this.vely = Math.floor(Math.random() * 2) + 0.5;
+
+        //Definindo direções diferentes para cada bola
+        this.dirx = (Math.random() * 10) > 5 ? 1 : 1;
+        this.diry = (Math.random() * 10) > 5 ? 1 : 1;
+
+        this.palco = palco;
+        this.arrayBolas = arrayBolas;
+
+        //Definindo um id diferente para cada bola
+        this.id = Date.now() + "_" + Math.floor(Math.random() * 100000000000);
+
+        this.desenhar();
+        this.controle = setInterval(this.controlar,10);
+        this.eu = document.getElementById(this.id);
+    }
+
+    //Função para saber a posição que esta a bola no array
+    minhaPos = () => {
+
+    }
+
+    //Função para remover a bola
+    remover = () => {
+
+    }
+
+    //Função que vai desenhar a bola
+    desenhar = () => {
+
+    }
+
+    //Função para controlar a bola
+    controlar = () => {
+
     }
 }
 
@@ -44,7 +82,7 @@ window.addEventListener("resize", (evt) => {
 btn_add.addEventListener("click", (evt) => {
     //Pegando o valor de bolinhas que quer ser colocadas
     const qtde = txt_qtde.value;
-    for(let i = 0; i < qtde; i++){
+    for (let i = 0; i < qtde; i++) {
         //Instanciar novas bolinhas
     }
 });
