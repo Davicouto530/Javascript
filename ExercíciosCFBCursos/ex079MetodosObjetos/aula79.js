86d6a872184af4ee8999b8e78e1d5aef62f9431e
Davi do Couto <coutodavi530@gmail.com>
2025-04-19 12:51:52 -0300

    Aula de métodos em objetos literais com o CFB Cursos.

---
+++ ExercíciosCFBCursos/ex079MetodosObjetos/aula79.js
@@ -0,0 +1,43 @@
+"use strict"; 
+
+/* 
+ *  Métodos Objetos literais
+ */
+
+const objetos = document.getElementById("objetos");
+
+//Criando um objeto base
+const computador = {
+    cpu: "",
+    ram: "",
+    ssd: "",
+    //Criando um método para exibir todas as propriedades
+    info:function(){
+        console.log(`CPU: ${this.cpu}`);
+        console.log(`RAM: ${this.ram}`);
+        console.log(`SSD: ${this.ssd}`);
+    }
+}
+
+computador.placaVideo="rtx1650";
+
+//Deletando uma propriedade do objeto
+delete(computador.ssd);
+
+//Clonando o objeto computador
+const c1 = Object.assign({}, computador);
+c1.info();
+
+//criando um novo objeto
+const c2 = Object.create(computador);
+c2.cpu = "i9";//Definindo as propriedades
+c2.ram = "64gb";//do objeto base
+c2.ssd = "1tb";
+c2.info();
+
+//Mesclando os objetos
+const o1 = {obj1: "1"}
+const o2 = {obj2: "2"}
+const o3 = {obj3: "3"}
+const o4 = Object.assign(o1,o2,o3);
+console.log(o4);
