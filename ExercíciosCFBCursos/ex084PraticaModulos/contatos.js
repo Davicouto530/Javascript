31eb1343e2633f494ba8f6fe5ca6f7e9c6728e3f
Davi do Couto <coutodavi530@gmail.com>
2025-04-22 21:26:12 -0300

    Adicionando os elementos na tela de acordo com o array, e colocando o botão de remover, para remover o item do array e do HTML.

--- ExercíciosCFBCursos/ex084PraticaModulos/contatos.js
+++ ExercíciosCFBCursos/ex084PraticaModulos/contatos.js
@@ -1,4 +1,4 @@
-"use strict"; 
+"use strict";
 
 /* 
  *  Arquivo módulo de contatos
@@ -10,18 +10,18 @@ import { contatos } from "./bancoContatos.js";
 //Objeto que vai as informações
 let contato = {
     //Método que vai retornar todos os contatos
-    getTodosContatos:function(){
+    getTodosContatos: function () {
         return contatos;
     },
 
     //Método que vai retornar só um contato especifico
-    getContato:function(ind_cont){
+    getContato: function (ind_cont) {
         return contatos[ind_cont];
     },
 
     //Método que vai adicionar o contato no banco de 
     //dados e no DOM, na lista de contatos no HTML
-    addContato:function(novoContato, destinoDOM){
+    addContato: function (novoContato, destinoDOM) {
         //Pegando as informações que veio do input, e colocando no
         //objeto, pra do objeto ir pro array do módulo "bancoContatos"
         const cont = {
@@ -31,29 +31,51 @@ let contato = {
         }
         contatos.push(cont)//Adicionando no array as informções
 
-        //Criando o elemento que vai div que vai na div lista
-        const div = document.createElement("div");
-        div.setAttribute("class", "contato");
+        destinoDOM.innerHTML = '';
 
-        //Criando o elemento "p" que vai receber o nome
-        const p_nome = document.createElement("p");
-        p_nome.innerHTML = novoContato.nome;
+        //Percorrendo o array e listando os conteúdos no HTML
+        contatos.forEach((c) => {
+            //Criando o elemento que vai div que vai na div lista
+            const div = document.createElement("div");
+            div.setAttribute("class", "contato");
 
-        //Criando o elemento "p" que vai receber o telefone
-        const p_telefone = document.createElement("p");
-        p_telefone.innerHTML = novoContato.telefone;
+            //Criando o elemento "p" que vai receber o nome
+            const p_nome = document.createElement("p");
+            p_nome.innerHTML = c.nome;
 
-        //Criando o elemento "p" que vai receber o email
-        const p_email = document.createElement("p");
-        p_email.innerHTML = novoContato.email;
-        
-        //Adicionando os "p" com as informações na "div"
-        div.appendChild(p_nome);
-        div.appendChild(p_telefone);
-        div.appendChild(p_email);
+            //Criando o elemento "p" que vai receber o telefone
+            const p_telefone = document.createElement("p");
+            p_telefone.innerHTML = c.telefone;
 
-        //Adicionando a "div" na lista no HTML
-        destinoDOM.appendChild(div);
+            //Criando o elemento "p" que vai receber o email
+            const p_email = document.createElement("p");
+            p_email.innerHTML = c.email;
+
+            const btn_lixeira = document.createElement("img");//Criando uma tag "img" 
+            btn_lixeira.setAttribute("src", "./lixeira.png");//Colocando a imagem dentro da nova tag "img"
+            btn_lixeira.setAttribute("class", "btn_lixeira");//Colocando uma class na nova tag "img"
+
+            //Colocando um evento de click, que quando clicar na img de lixeira, vai remover a tag div
+            btn_lixeira.addEventListener("click", (evt) => {
+                evt.target.parentNode.remove(); // remove da tela
+                //O ".removeChild" serve para remover uma tag do HTML. Indicando que vai remover do container 
+                //"destinoDOM", e o "evt.target.parentNode" vê qual foi clicado, qual for clicado, vai ser removido.
+
+                // Encontra o índice do contato atual (c) dentro do array "contatos"
+                const indexRemover = contatos.findIndex(el => el === c);
+                contatos.splice(indexRemover, 1); // Remove 1 elemento a partir do índice encontrado
+                console.log(contatos);
+            });
+
+            //Adicionando os "p" com as informações na "div"
+            div.appendChild(p_nome);
+            div.appendChild(p_telefone);
+            div.appendChild(p_email);
+            div.appendChild(btn_lixeira)
+
+            //Adicionando a "div" na lista no HTML
+            destinoDOM.appendChild(div);
+        })
     }
 }
 
