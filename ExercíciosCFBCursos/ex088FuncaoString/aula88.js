9c9323124c62e0ed34069c08a88ee6319654cd47
Davi do Couto <coutodavi530@gmail.com>
2025-04-25 15:52:30 -0300

    Aula de funções de string com CFB Cursos.

---
+++ ExercíciosCFBCursos/ex088FuncaoString/aula88.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/* 
+ *  Funções para Strings
+ */
+
+//Instanciando uma classe String com o valor "bruno"
+let nome = new String("Bruno Pinho Campos");
+let canal = new String("CFBCursos");
+
+console.log(nome);
+console.log(typeof(nome));
+
+//O "charAt" serve para indicar a posição 
+//da letra que você quer ver
+console.log(nome.charAt(0));
+
+//O "charCodeAt" serve para indicar a posição 
+//da letra que você quer ver o código
+console.log(nome.charCodeAt(0));
+
+//O "concat" serve para juntar duas strings, concatenar
+console.log(nome.concat(canal));
+
+//O "indexOf" percorre a string e retorna a posição do primeiro que ele 
+//achar do que foi passado nos parenteses, se não tiver, retorna "-1"
+console.log(nome.indexOf("o"));
+
+//O "lastIndexOf" percorre a string e retorna a posição do ultimo que ele
+//achar do que foi passado nos parenteses, se não tiver, retorna "-1"
+console.log(nome.lastIndexOf("o"));
