509cc8bbe7b85d04a4f4d6138c5040670d7b5ab8
Davi do Couto <coutodavi530@gmail.com>
2025-04-27 19:32:17 -0300

    Colocando botão para copiar a senha, e arrumando o gerador de senhas para gerar o tanto certo passado pelo input.

--- MiniProjetos/GeradorSenhas/script.js
+++ MiniProjetos/GeradorSenhas/script.js
@@ -1,7 +1,9 @@
+const divSenha = document.getElementById("divSenha");
 const tamanho = document.getElementById("tamanho");
 const inputValor = document.getElementById("valor");
 const btnGerarSenha = document.getElementById("gerarSenha");
-const res = document.getElementById("res");
+const pSenha = document.getElementById("pSenha");
+
 
 inputValor.addEventListener("click", () => {
     tamanho.innerHTML = inputValor.value;
@@ -10,12 +12,31 @@ inputValor.addEventListener("click", () => {
 let caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+[]{}|;:,.<>?/";
 
 btnGerarSenha.addEventListener("click", () => {
-    let valorConvertido = parseInt(inputValor.value)
+    let valorConvertido = parseInt(inputValor.value);
+    divSenha.innerHTML = '';
+    // console.log(valorConvertido)
     let senhaGerada = '';
     let sorteados = '';
-    for(let i = 0; i < valorConvertido; i++){
-        sorteados = Math.floor(Math.random()*caracteres.length);
+    for (let i = 0; i < valorConvertido; i++) {
+        sorteados = Math.floor(Math.random() * caracteres.length);
+        // console.log(sorteados)
         senhaGerada += caracteres[sorteados];
+        console.log(senhaGerada)
     }
-    res.innerHTML = senhaGerada;
+
+    const res = document.createElement("div");
+    res.setAttribute("class", "res");
+    res.innerHTML = '';
+    pSenha.innerHTML = "Sua senha gerada foi:";
+    res.textContent = senhaGerada;
+
+    const copiar = document.createElement("img");
+    copiar.setAttribute("src", "copiar.png");
+    copiar.setAttribute("class", "copiar");
+    copiar.addEventListener("click", (evt) => {
+        navigator.clipboard.writeText(res.innerHTML);
+    });
+    divSenha.appendChild(copiar);
+    divSenha.appendChild(pSenha);
+    divSenha.appendChild(res);
 });
