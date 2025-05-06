6d07b977b6a0882c1492fa6aeeaf8204c6b88727
Davi do Couto <162334727+Davicouto530@users.noreply.github.com>
2025-05-06 20:59:11 -0300

    Implementando quando chegar no final da tela, parar o carro com o CFB Cursos.

--- ExercíciosCFBCursos/ex092AnimacaoP2/aula92.js
+++ ExercíciosCFBCursos/ex092AnimacaoP2/aula92.js
@@ -13,15 +13,38 @@ const btn_direita = document.getElementById("btn_direita");
 //Função que prepara o carro pra se mexer na tela
 const init = () => {
     //Tô dizendo pro carro que ele pode se mover e coloco ele lá no comecinho, bem do lado esquerdo
-    carro.style = "position:relative;left:0px";
+    carro.style = "position:relative;left:0px;width:100px";//E adicionando um tamanho para ele "width"
+
+    //Variável que pega a largura máxima da janela, e subtrai com o tamaho do carro
+    tamMax = window.innerWidth - parseInt(carro.style.width);
 }
 
 let anima = null; //Isso aqui é tipo um controle remoto da animação, pra parar ou mudar quando quiser
+let tamMax = null;//Variável que está dentro da função "init", que pega a largura da tela
 
 //Função que faz o carro andar de pouquinho em pouquinho
 const mover = (dir) => {
-    //Pega onde o carro tá, soma ou diminui 10 (depende se vai pra esquerda ou direita)
-    carro.style.left = parseInt(carro.style.left) + (10 * dir) + "px";
+    //Verificando quando o carro se move
+    if (dir > 0) {
+        // se "dir" for maior que 0zero, ta se movendo para a direita
+        if (parseInt(carro.style.left) <= tamMax) {
+            //verifica se carro é menor ou igual a "tamMax", se for, realiza o debaixo
+            carro.style.left = parseInt(carro.style.left) + (10 * dir) + "px";
+            //Pega onde o carro tá, soma ou diminui 10 (depende se vai pra esquerda ou direita)
+        }else {
+            clearInterval(anima);
+        }
+    } else if (dir < 0) {
+        // se "dir" for menor que 0zero, ta se movendo para a esquerda
+        if (parseInt(carro.style.left) >= tamMax) {
+            //verifica se carro é maior ou igual a "tamMax", se for, realiza o debaixo
+            carro.style.left = parseInt(carro.style.left) + (10 * dir) + "px";
+            //Pega onde o carro tá, soma ou diminui 10 (depende se vai pra esquerda ou direita)
+        }else {
+            clearInterval(anima);
+        }//Quando chegar no final, limpa o setInterval
+    }
+    console.log(tamMax);
 
     //----------Jeito 2 de fazer com o "setTimeout"----------
     // anima = setTimeout(mover, 20, dir);
@@ -58,5 +81,10 @@ btn_direita.addEventListener("click", () => {
 });
 
 //Quando a página abrir, já coloca o carro no lugar certinho pra começar a brincadeira
-window.onload = init;
-// window.addEventListener("load", init());
+window.addEventListener("load", init());
+window.addEventListener("resize", () => {
+    //Sempre que a tela for redimensionada, faz a função
+    tamMax = window.innerWidth - parseInt(carro.style.width);
+});
+
+// window.onload = init; //Segundo jeito de executar a função quando abrir a página
