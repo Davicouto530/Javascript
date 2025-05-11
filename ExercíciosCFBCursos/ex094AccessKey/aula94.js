f08e6ae269d87bd77bd50f15484deefbe6327d29
Davi do Couto <162334727+Davicouto530@users.noreply.github.com>
2025-05-10 21:50:44 -0300

    Aula de accessKey com o CFB Cursos, e desafio de lógica.

--- ExercíciosCFBCursos/ex094AccessKey/aula94.js
+++ ExercíciosCFBCursos/ex094AccessKey/aula94.js
@@ -1,9 +1,9 @@
 "use strict";
 
 /* 
- *  Função GetBoundingClientRect 
- * getBoundingClientRect() retorna um objeto com a posição (x, y)
- * e o tamanho (largura e altura) de um elemento na tela, em pixels.
+ *  Função AccessKey 
+ *  O "accessKey" serve para definir uma tecla de atalho que vai executar uma função
+ *  Para executar a fução, tem que segurar a tecla "alt" e a tecla que você definir
  */
 
 const q1 = document.getElementById("q1");
@@ -12,6 +12,12 @@ const posx = document.getElementById("posx");
 const posy = document.getElementById("posy");
 const largura = document.getElementById("largura");
 const altura = document.getElementById("altura");
+
+q1.accessKey = "b";//Definindo que quando clicar no "alt" + "b", vai executar a função do "q1"
+q2.accessKey = "n";//Definindo que quando clicar no "alt" + "n", vai executar a função do "q2"
+
+console.log(`Tecla de atalho do q1: ${q1.accessKey}`);//Exibindo quais são as teclas de atalho  
+console.log(`Tecla de atalho do q2: ${q2.accessKey}`);
 
 // Função que exibe as informações de posição e tamanho de um elemento na tela
 const info = (el) => {
