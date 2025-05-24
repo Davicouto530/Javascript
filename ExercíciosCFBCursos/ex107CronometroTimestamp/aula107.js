b99be1a4237489a2637bc56945d4bb44d6421b52
Davi do Couto <162334727+Davicouto530@users.noreply.github.com>
2025-05-24 19:13:51 -0300

    Criando os botões de iniciar, parar e zerar a contagem do cronômetro com o CFB Cursos.

--- ExercíciosCFBCursos/ex107CronometroTimestamp/aula107.js
+++ ExercíciosCFBCursos/ex107CronometroTimestamp/aula107.js
@@ -3,8 +3,12 @@
  */
 
 const timer = document.getElementById("timer");
+const btn_iniciar = document.getElementById("btn_iniciar");
+const btn_parar = document.getElementById("btn_parar");
+const btn_zerar = document.getElementById("btn_zerar");
 
-const tmpIni = Date.now(); // pega o timestamp do momento que começou
+let intervalo = null;
+let tmpIni = null;
 console.log(tmpIni);
 
 const contador = () => {
@@ -20,21 +24,35 @@ const contador = () => {
 // função que converte segundos pra horas:minutos:segundos
 const converter = (seg) => {
     // divide os segundos por 3600 (1 hora tem 3600 segundos) e pega só a parte inteira (horas)
-    const hora = Math.floor(seg / 3600); 
+    const hora = Math.floor(seg / 3600);
 
     // pega o resto da divisão por 3600, que são os segundos que não formaram uma hora
-    const resto = seg % 3600; 
+    const resto = seg % 3600;
 
     // divide esse resto por 60 (1 minuto tem 60 segundos) e pega só a parte inteira (minutos)
-    const minuto = Math.floor(resto / 60); 
+    const minuto = Math.floor(resto / 60);
 
     // pega o resto da divisão por 60, que são os segundos que sobraram depois de tirar as horas e os minutos
-    const segundo = Math.floor(resto % 60); 
+    const segundo = Math.floor(resto % 60);
 
     // formata pra ficar no padrão 00:00:00
-    const formatado = `${hora < 10 ? "0" + hora : hora}:${minuto < 10 ? "0" + minuto : minuto}:${segundo < 10 ? "0" + segundo : segundo}`;
+    const tempoFormatado = `${hora < 10 ? "0" + hora : hora}:${minuto < 10 ? "0" + minuto : minuto}:${segundo < 10 ? "0" + segundo : segundo}`;
 
-    return formatado; // retorna o tempo formatado
+    return tempoFormatado; // retorna o tempo formatado
 }
 
-setInterval(contador, 1000); // executa a função contador a cada 1 segundo
+btn_iniciar.addEventListener("click", () => {
+    tmpIni = Date.now(); // pega o timestamp do momento que começou
+    intervalo = setInterval(contador, 1000); // executa a função contador a cada 1 segundo
+});
+
+btn_parar.addEventListener("click", () => {
+    // Quando clicar no botão de parar, para a contagem do "setInterval"
+    clearInterval(intervalo);
+});
+
+btn_zerar.addEventListener("click", (evt) => {
+    tmpIni = Date.now();
+    timer.innerHTML = "00:00:00";
+    clearInterval(intervalo);
+});
