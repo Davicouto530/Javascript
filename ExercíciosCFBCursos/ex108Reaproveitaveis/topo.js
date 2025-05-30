ac78736ed359a659fe3db137b136aa456567158c
Davi do Couto <162334727+Davicouto530@users.noreply.github.com>
2025-05-29 21:57:35 -0300

    Aula de header reaproveitável com js pelo CFB Cursos.

---
+++ ExercíciosCFBCursos/ex108Reaproveitaveis/topo.js
@@ -0,0 +1,30 @@
+/*
+ * Header Reaproveitável
+ */
+
+const head = document.head;
+const body = document.body;
+
+const estilo = "<link rel='stylesheet' href='style.css'>";
+head.innerHTML += estilo;
+
+const topo = document.createElement("div");
+topo.setAttribute("id","topo");
+topo.setAttribute("class","topo");
+
+body.prepend(topo);
+
+const logo = 
+    "<div id='logo' class='logo''>"+
+        "<img src='logoCfbCursos.jpg' title='CFBCursos'/>"+
+    "</div>";
+
+topo.innerHTML += logo;
+
+const login = 
+    "<div id='login' class='login''>"+
+        "<p id='matricula'>Matrícula: <span></span></p>"+
+        "<p id='nome'>Nome: <span></span></p>"+
+    "</div>";
+
+topo.innerHTML += login;
