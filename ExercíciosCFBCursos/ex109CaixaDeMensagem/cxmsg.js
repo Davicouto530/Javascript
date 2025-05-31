4d6ad67f28a6deef2720052a1b61d800e545dd35
Davi do Couto <162334727+Davicouto530@users.noreply.github.com>
2025-05-31 16:05:06 -0300

    Fazendo as divs e o titulo da caixa com o CFB Cursos.

--- ExercíciosCFBCursos/ex109CaixaDeMensagem/cxmsg.js
+++ ExercíciosCFBCursos/ex109CaixaDeMensagem/cxmsg.js
@@ -17,7 +17,44 @@ class Cxmsg {
 
     // Método para exibir a caixa de mensagem
     mostrar = () => {
-        console.log(this.texto); // Por enquanto, só exibe o texto no console
+        this.divmsg = document.createElement("div");
+        const estilo_divmsg =
+            "display: flex;" +
+            "justify-content: center;" +
+            "align-items: center;" +
+            "position: absolute;" +
+            "top: 0px;" +
+            "left: 0px;" +
+            "width: 100%;" +
+            "height: 100vh;" +
+            "background-color: rgba(0,0,0,0.7);"
+        this.divmsg.setAttribute("id", "divmsg")
+        this.divmsg.setAttribute("style", estilo_divmsg)
+        this.destino.prepend(this.divmsg)
+
+        const estilo_areaCxmsg =
+            "display: flex;" +
+            "justify-content: flex-start;" +
+            "align-items: flex-start;" +
+            "flex-direction: column;" +
+            "width: 300px;"
+        const areaCxmsg = document.createElement("div");
+        areaCxmsg.setAttribute("style", estilo_areaCxmsg)
+        this.divmsg.appendChild(areaCxmsg)
+
+        const estilo_tituloCxmsg =
+            "display: flex;" +
+            "justify-content: flex-start;" +
+            "align-items: center;" +
+            "width: 100%;" +
+            "background-color:"+this.cor+";"+
+            "color: #fff;"+
+            "padding: 5px;"+
+            "border-radius: 5px 5px 0px 0px"
+        const tituloCxmsg = document.createElement("div");
+        tituloCxmsg.setAttribute("style", estilo_tituloCxmsg)
+        tituloCxmsg.innerHTML = this.titulo
+        areaCxmsg.appendChild(tituloCxmsg)
     }
 
     // Método para ocultar a caixa (ainda vazio)
